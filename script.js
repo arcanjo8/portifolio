18c84e91b8a695ae194df8a9cd2bc35bfc98e75c
arcanjo8 <jailson.a.melo@gmail.com>
2022-06-07 20:14:55 -0300

    :package:menu sidebar

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".hamburguer").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+);
+
+
+// tempo 45:52 -- 46:51 aula 03
